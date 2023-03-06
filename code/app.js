adb94c028ccc6808f5cebc5cc3502deb764da0c5
MagdyNasr41 <90320433+MagdyNasr41@users.noreply.github.com>
2023-03-06 08:39:57 +0200

    game is done

--- code/app.js
+++ code/app.js
@@ -4,9 +4,9 @@ const timeLeft = document.querySelector('#time-left');
 const score = document.querySelector('#score');
 
 let result = 0;
-let hitPosition
-let currentTime = 60
-let timerId = null
+let hitPosition;
+let currentTime = 60;
+let timerId = null;
 
 
 function randomSquare() {
@@ -21,7 +21,7 @@ function randomSquare() {
 };
 
 squares.forEach(square => {
-    square.addEventListener('movedown', () => {
+    square.addEventListener('mousedown', () => {
         if (square.id == hitPosition) {
             result++;
             score.textContent = result;
@@ -31,13 +31,19 @@ squares.forEach(square => {
 });
 
 function moveMole() {
-    let timerId = null;
-    timerId = setInterval(randomSquare, 500);
+    timerId = setInterval(randomSquare, 5000);
 };
 
 moveMole()
 
+function countDown() {
+    currentTime--;
+    timeLeft.textContent = currentTime;
 
+    if (currentTime === 0) {
+        clearInterval(countDownTimerId);
+        alert("GAME OVER! your final score is : " + result)
+    }
+}
 
-
-
+let countDownTimerId = setInterval(countDown, 1000)
